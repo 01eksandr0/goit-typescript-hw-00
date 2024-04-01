d5896ff47099ed12b1607eb96c75add44ccbb095
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-04-01 12:49:56 +0300

    1

---
+++ dist/concatination.js
@@ -0,0 +1,2 @@
+export const concatenation = (firstWord, secondWord) => console.log(`${firstWord} ${secondWord}`);
+//# sourceMappingURL=concatination.js.map
---
+++ src/concatination.ts
@@ -0,0 +1,2 @@
+export const concatenation = (firstWord: string, secondWord: string) =>
+  console.log(`${firstWord} ${secondWord}`);
